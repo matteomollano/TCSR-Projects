5e986869d0d35b4c9aaa40ba8f5a3ed9f981e8af
Matteo <matteomollano@gmail.com>
2026-01-31 17:48:59 -0500

    implemented addExpense() and removeExpense() methods

--- Java/ExpenseTracker/ExpenseTracker.java
+++ Java/ExpenseTracker/ExpenseTracker.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
+/*
+    List that stores multiple Expense objects
+    How to create: Expense obj1 = new Expense();
+    ArrayList -> [obj1, obj2, obj3, obj4]
+*/
 public class ExpenseTracker {
     // TODO: Declare an ArrayList to store Expense objects
     // This will be the main collection that holds all user expenses
@@ -13,11 +18,23 @@ public class ExpenseTracker {
     // TODO: addExpense() method
     // - Takes an Expense object as a parameter
     // - Adds it to the collection
+    public void addExpense(Expense expense) {
+        this.expenses.add(expense);
+    }
     
     // TODO: removeExpense() method
-    // - Takes an identifier (index, description, or date) as parameter
+    // - Takes an identifier (expenseID) as parameter
     // - Removes the expense from the collection
-    
+    public void removeExpense(String expenseID) {
+        for (Expense expense : this.expenses) {
+            String id = expense.getExpenseID();
+            if (expenseID.equals(id)) {
+                this.expenses.remove(expense);
+                break;
+            }
+        }
+    }
+
     // TODO: displayExpenses() method
     // - Display all tracked expenses
     
