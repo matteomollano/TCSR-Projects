6bf60af823691f2edd1b4730d35a1b239bae1ffe
Matteo <matteomollano@gmail.com>
2026-02-07 14:58:21 -0500

    added movement for turtle during today's code class

--- Python/TurtleTreasureHunt/treasure_hunt.py
+++ Python/TurtleTreasureHunt/treasure_hunt.py
@@ -15,4 +15,31 @@ treasure.penup()
 treasure.speed(0)
 treasure.goto(random.randint(-250, 250), random.randint(-250, 250))
 
+# 0 = right
+# 90 = up
+# 180 = left
+# 270 = down
+
+def right():
+    tortoise.setheading(0)
+    tortoise.forward(20)
+    
+def left():
+    tortoise.setheading(180)
+    tortoise.forward(20)
+
+def up():
+    tortoise.setheading(90)
+    tortoise.forward(20)
+
+def down():
+    tortoise.setheading(270)
+    tortoise.forward(20)
+
+screen.listen()
+screen.onkey(right, "Right")
+screen.onkey(left, "Left")
+screen.onkey(up, "Up")
+screen.onkey(down, "Down")
+
 screen.mainloop()
