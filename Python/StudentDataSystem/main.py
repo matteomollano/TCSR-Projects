bd8416bd5af15dedf30fc25cd39e26e0165a5476
Matteo <matteomollano@gmail.com>
2026-02-14 17:35:59 -0500

    2/14: implemented terminal UI and tested functions

--- Python/StudentDataSystem/main.py
+++ Python/StudentDataSystem/main.py
@@ -32,33 +32,60 @@ def display_student(data):
     print("Courses:")
     for subject, letter_grade in courses.items():
         print(f"{subject}: {letter_grade}")
-    
+        
 def search_student(student_id):
     print(f"Searching for student with ID {student_id}")
     student_data = students[student_id]
     display_student(student_data)
-
+    
 def display_all_students():
     for student_id, data in students.items():
         print(f"Student ID: {student_id}")
         display_student(data)
+        print()
         
 def main():
-    name = input("Enter your name: ")
-    age = int(input("Enter your age: "))
-    grade = input("Enter your school grade: ")
-    
-    courses = {}
+    print("Welcome to Student Data System")
     while True:
-        subject = input("Enter a subject: ")
-        letter_grade = input("Enter letter grade: ")
-        courses[subject] = letter_grade
+        print("1. Search for student")
+        print("2. Add new student")
+        print("3. Delete student")
+        print("4. Display all students")
+        prompt = input("Choose an option: ")
         
-        again = input("Do you want to enter another course? (y/n): ")
-        if again != "y":
-            break
-    
-    add_student(name, age, grade, courses)
-    display_all_students()
+        if prompt == "1":
+            print("You chose search")
+            student_id = input("Enter student ID to search for: ")
+            search_student(student_id)
+            
+        elif prompt == "2":
+            print("You chose add")
+            name = input("Enter name: ")
+            age = input("Enter age: ")
+            grade = input("Enter grade: ")
+            courses = {}
+            for i in range(10):
+                subject = input("Enter subject: ")
+                letter_grade = input("Enter letter grade: ")
+                courses[subject] = letter_grade
+
+                if len(courses) >= 5:
+                    again = input("Another course? y/n").lower()
+                    if again == "n":
+                        break  
+             
+            add_student(name, age, grade, courses)
+                
+        elif prompt == "3":
+            print("You chose delete")
+            student_id = input("Enter student ID to delete: ")
+            delete_student(student_id)
+            
+        elif prompt == "4":
+            print("You chose display all")
+            display_all_students()
+            
+        else:
+            print("Error")
     
 main()
