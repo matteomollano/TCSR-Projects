73c4122dcf7c821e50f7601d9506c32ab4969533
matteomollano <78442437+matteomollano@users.noreply.github.com>
2026-08-15 16:58:32 -0400

    2026-08-15 (#6)
    * 2026-08-15: update flask weather website
    
    * 2026-08-15: Calculator GUI with tkinter

--- Python/WeatherWebsite/main.py
+++ Python/WeatherWebsite/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from geopy.geocoders import Nominatim
 import requests
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -14,9 +15,31 @@ def home():
     # retrieve weekly weather forecast via weather.gov API
     response = requests.get(f"https://api.weather.gov/points/{lat},{long}").json()
     forecast_url = response["properties"]["forecast"]
-    forecast_data = requests.get(forecast_url).json()
+    forecast_data = requests.get(forecast_url).json()["properties"]["periods"]
     
-    return render_template("index.html", name="test")
+    cleaned_data = []
+    for period in forecast_data:
+        
+        date = datetime.fromisoformat(period["startTime"]).strftime("%m/%d/%Y")
+        start_time = datetime.fromisoformat(period["startTime"]).strftime("%-I:%M %p")
+        end_time = datetime.fromisoformat(period["endTime"]).strftime("%-I:%M %p")
+        
+        cleaned_data.append({
+            "day": period["name"],
+            "date": date,
+            "start_time": start_time,
+            "end_time": end_time,
+            "time_zone": "EST",
+            "temp": period["temperature"],
+            "temp_unit": period["temperatureUnit"],
+            "wind_speed": period["windSpeed"],
+            "wind_direction": period["windDirection"],
+            "description": period["detailedForecast"],
+            "precipitation": period["probabilityOfPrecipitation"]["value"],
+            "icon": period["icon"]
+        })
+        
+    return render_template("index.html", data=cleaned_data)
 
 
 if __name__ == "__main__":
