4a6f2fc2ee0140d07c526e564836ccd98c6aed6a
matteomollano <78442437+matteomollano@users.noreply.github.com>
2026-08-29 18:12:25 -0400

    2026-08-29: work in progress weather website with python flask (#7)
    * 2026-08-29: work in progress weather website
    
    * Center weather-content section and remove un-used code

--- Python/WeatherWebsite/main.py
+++ Python/WeatherWebsite/main.py
@@ -1,45 +1,18 @@
-from flask import Flask, render_template
-from geopy.geocoders import Nominatim
-import requests
-from datetime import datetime
+from flask import Flask, render_template, request
+from get_weather import get_weather_data
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # get latitude and longitude using geopy
-    geolocator = Nominatim(user_agent="my-app")
-    location = geolocator.geocode("Roslyn, New York")
-    lat, long = location.latitude, location.longitude
+    return render_template("index.html")
+
+@app.route("/get_weather", methods=["GET"])
+def get_weather():
+    city = request.args["city"].strip()
+    data, error = get_weather_data(city)
     
-    # retrieve weekly weather forecast via weather.gov API
-    response = requests.get(f"https://api.weather.gov/points/{lat},{long}").json()
-    forecast_url = response["properties"]["forecast"]
-    forecast_data = requests.get(forecast_url).json()["properties"]["periods"]
-    
-    cleaned_data = []
-    for period in forecast_data:
-        
-        date = datetime.fromisoformat(period["startTime"]).strftime("%m/%d/%Y")
-        start_time = datetime.fromisoformat(period["startTime"]).strftime("%-I:%M %p")
-        end_time = datetime.fromisoformat(period["endTime"]).strftime("%-I:%M %p")
-        
-        cleaned_data.append({
-            "day": period["name"],
-            "date": date,
-            "start_time": start_time,
-            "end_time": end_time,
-            "time_zone": "EST",
-            "temp": period["temperature"],
-            "temp_unit": period["temperatureUnit"],
-            "wind_speed": period["windSpeed"],
-            "wind_direction": period["windDirection"],
-            "description": period["detailedForecast"],
-            "precipitation": period["probabilityOfPrecipitation"]["value"],
-            "icon": period["icon"]
-        })
-        
-    return render_template("index.html", data=cleaned_data)
+    return render_template("index.html", data=data, city=city, error=error)
 
 
 if __name__ == "__main__":
